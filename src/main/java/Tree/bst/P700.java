a8ca8b11058232b147d1ff03507819f0c6eeebfc
vishnugupta <guptavishnunitsk@gmail.com>
2026-03-29 22:42:27 +0530

    700. Search in a Binary Search Tree

---
+++ src/main/java/Tree/bst/P700.java
@@ -0,0 +1,13 @@
+package Tree.bst;
+
+import Tree.TreeNode;
+
+public class P700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return root;
+        if(root.val == val) return root;
+        if(val > root.val) return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+}

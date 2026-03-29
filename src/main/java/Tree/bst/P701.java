2ab113c03bb8ba477478aac2b6b1e996371399c5
vishnugupta <guptavishnunitsk@gmail.com>
2026-03-29 23:07:13 +0530

    701. Insert into a Binary Search Tree

---
+++ src/main/java/Tree/bst/P701.java
@@ -0,0 +1,23 @@
+package Tree.bst;
+
+import Tree.TreeNode;
+
+public class P701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        if (root.val > val) {
+            if (root.left != null)
+                insertIntoBST(root.left, val);
+            else {
+                root.left = new TreeNode(val);
+            }
+        }
+        if (root.val < val) {
+            if (root.right != null)
+                insertIntoBST(root.right, val);
+            else
+                root.right = new TreeNode(val);
+        }
+        return root;
+    }
+}
